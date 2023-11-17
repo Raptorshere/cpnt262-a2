6423f983809e639db7dc744e64474159e2b45320
Kevin Fula <kevin.fula@edu.sait.ca>
2023-11-16 23:57:49 -0700

    attempt of adding in images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,6 +28,19 @@
 				Launch Documentation
 			</a>
 		</div>
+		
+<section class="grid grid-cols-2 md:grid-cols-3 gap-4">
+	<div>
+		<img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop" alt="">
+	</div>
+	<div>
+		<img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1553184570-557b84a3a308?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY2NTF8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop" alt="">
+	</div>
+	<div>
+		<img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop" alt="">
+	</div>
+</section>
+
 		<div class="space-y-2">
 			<p>Try editing the following:</p>
 			<p><code class="code">/src/routes/+layout.svelte</code></p>
